b112057f6e957c431873d0fa89b5b5898784e5f5
Andrew <andrewn0729@student.k12.tulsaschools.org>
2021-09-15 09:27:38 -0500

    added moving x and o

---
+++ ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  TicTacToeTwo
+//
+//  Created by AMStudent on 9/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        NavigationView {
+        Home()
+            .navigationTitle("Tic Tac Toe")
+            .preferredColorScheme(.dark)
+        }
+    }
+}
+
+struct Home: View {
+    
+    @State var moves: [String] = Array(repeating: "", count: 9)
+    @State var isPlaying = false
+   
+    var body: some View {
+        VStack {
+    
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 15), count: 3), spacing: 15) {
+                
+                
+                ForEach(0..<9, id: \.self) {
+                    index in
+                    
+                    ZStack {
+                        Color.white
+                        
+                        Text(moves[index])
+                            .font(.system(size:55))
+                            .fontWeight(.heavy)
+                            .foregroundColor(.black)
+                    }
+                    
+                   
+                        .frame(width: getWidth(), height: getWidth())
+                        .cornerRadius(8)
+                    .onTapGesture(perform: {
+                        withAnimation(Animation.easeIn(duration: 0.5)) {
+                            
+                            moves[index] = isPlaying ? "X" : "O"
+                            isPlaying.toggle()
+                        }
+                    })
+                }
+            }
+            .padding(15)
+            
+        }
+    }
+    func getWidth() -> CGFloat {
+        let width = UIScreen.main.bounds.width - (30 + 30)
+        
+        return width / 3
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
